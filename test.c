5756faab37457a78e00efeca73568fe1fca5c7cf
Adam F Froio <afroio@gmail.com>
2017-08-16 22:13:20 -0700

    fixed the program

--- test.c
+++ test.c
@@ -1,13 +1,18 @@
 // main 
 #include <stdio.h>
 
+
+int feature1(char string[]);
+
 int main(void) {
 	printf("main\n");
-	feature1();
+	feature1("feature1");
+	return 0;
 }
 
 // feature1
-int feature1(void)
+int feature1(char string[])
 {
-	printf("feature1\n");
+	printf("%s\n", string);
+	return 0;
 }
